71be472a6be7308db03ad8a1bb6322f7567e0235
girmy008 <164983725+girmy008@users.noreply.github.com>
2025-03-20 11:18:24 +1030

    Item constructors/methods
    Added constructors to item and methods to get, set and return variables according to varying parameters.

--- src/students/items/Item.java
+++ src/students/items/Item.java
@@ -1,6 +1,27 @@
 package students.items;
 
 public class Item {
-
+	int age = 0;
+	int maturationAge;
+	int deathAge;
+	int maturation;
+	int monetaryValue;
 	
+	public void tick(){
+		age++;}
+
+	public void setAge(int age) {
+		this.age = age;}
+	
+	public boolean died(){
+		boolean dead = false;
+		if(age>deathAge) {
+			dead = true;}
+		return dead;}
+
+	public int getValue(){
+		if (age>maturation){
+		return monetaryValue;}
+	}
+
 }
