6fb97a406b4ab097a5a6c6adee7291642ea41c49
girmy008 <164983725+girmy008@users.noreply.github.com>
2025-04-07 20:16:01 +0930

    near completed input validator
    made strings convert to integers from user input all the while sending errors if not possible..

--- src/students/Farm.java
+++ src/students/Farm.java
@@ -30,13 +30,28 @@ public class Farm {
 				if(!input.substring(0,2).equals("t ") && !input.substring(0,2).equals("h ") && !input.substring(0,2).equals("p ")){
 					System.out.println("invalid input");
 				}
-			}
+				String coords = input.substring(2);
+				String[] coords1 = coords.split(" ");
+				String[] validChars = {"1","2","3","4","5","6","7","8","9","0"," "};
+				if (coords1.length!=2) {
+					System.out.println("invalid");
+				}
+				else if (coords1.length==2) {
+					try {
+					int xCoord = Integer.parseInt(coords1[0]);
+					int yCoord = Integer.parseInt(coords1[1]);
+					}catch(Exception e){
+						System.out.println("invalid");
+					}
+				}
+				}
+			
 			
 			beerenburg.tick();
-			
+		}
 			
 		}
 		
 		
-	}
 }
+
